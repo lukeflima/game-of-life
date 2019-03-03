9e813d265f048dd93acf775a2bda7d272b60b2e5
Lucas Lima <lukeflima@gmail.com>
2019-03-03 06:02:42 -0300

    Cleaning up

--- gameoflife.py
+++ gameoflife.py
@@ -2,42 +2,52 @@ import numpy as np
 import time
 import os
 
-def get_nei_num(index):
-    return np.sum(grid[index[0]-1:index[0]+2,index[1]-1:index[1]+2]) - grid[index[0], index[1]]
+'''
+Returns a list with the number of neighbors of each cell
+'''
+def get_neighbors_num(cell):
+    return np.sum(grid[cell[0]-1:cell[0]+2,cell[1]-1:cell[1]+2]) - grid[cell[0], cell[1]]
 
-
-def get_next_status(index_and_nei):
-    alive = index_and_nei[0]
-    num_nei = index_and_nei[1]
-    #print(alive, num_nei)
+'''
+Return the next state of the cell
+'''
+def get_next_status(cell_and_neighbors):
+    alive = cell_and_neighbors[0]
+    num_neighbors = cell_and_neighbors[1]
+    #print(alive, num_neighbors)
     if alive:
-        if num_nei < 2:
+        if num_neighbors < 2:
             return 0
-        elif num_nei >= 2 and num_nei <= 3:
+        elif num_neighbors >= 2 and num_neighbors <= 3:
             return 1
-        elif num_nei > 3:
+        elif num_neighbors > 3:
             return 0
     elif not alive:
-        if num_nei == 3:
+        if num_neighbors == 3:
             return 1
     return alive
 
-
+'''
+Return the next grid
+'''
 def get_next_grid(grid):
-    list_indexes = [[[i,j] for j in range(HEIGHT)] for i in range(WIDTH)]
-    num_nei = np.array([list(map(get_nei_num, x)) for x in list_indexes])
-    next_grid = np.array([list(map(get_next_status, x)) for x in [[[grid[i,j], num_nei[i,j]] for j in range(HEIGHT)] for i in range(WIDTH)]])
+    list_cells = [[[i,j] for j in range(HEIGHT)] for i in range(WIDTH)]
+    num_neighbors = np.array([list(map(get_neighbors_num, x)) for x in list_cells])
+    next_grid = np.array([list(map(get_next_status, x)) for x in [[[grid[i,j], num_neighbors[i,j]] for j in range(HEIGHT)] for i in range(WIDTH)]])
     return next_grid
 
-
+'''
+Return a new random grid
+'''
 def genarate_random_grid():
     return np.round(np.random.random([WIDTH,HEIGHT])).astype('int')
 
-                                                     
+
 rows, columns = os.popen('stty size', 'r').read().split()
 WIDTH, HEIGHT = int(rows), int(columns)
 
 grid = genarate_random_grid()
+# Try and finally for nice exit when CTRL + C
 try:
     os.system('clear')
     time.sleep(0.5)
